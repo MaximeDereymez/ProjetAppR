6ecbb7991307cadbe31b665bf8523e1c2d7d3909
Maxime Dereymez <dereymez.maxime@gmail.com>
2017-04-04 17:03:01 +0200

    Annuaire + Chaine + LookForHotel

---
+++ src/hotel/Annuaire.java
@@ -0,0 +1,54 @@
+package hotel;
+
+import java.io.File;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import jus.aor.mobilagent.kernel._Service;
+
+public class Annuaire extends UnicastRemoteObject implements _Annuaire, _Service<Numero> {
+	protected HashMap<String, String> annuaire;
+	
+	protected Annuaire(Object[] args) throws ParserConfigurationException, SAXException, IOException {
+		super();
+		/* Récupération de l'annuaire dans le fichier xml */
+		DocumentBuilder docBuilder = null;
+		Document doc=null;
+		docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		doc = docBuilder.parse(new File(args[0].toString()));
+
+		String name, numero;
+		NodeList list = doc.getElementsByTagName("Telephone");
+		NamedNodeMap attrs;
+		annuaire = new HashMap<>();
+		/* acquisition de toutes les entrées de l'annuaire */
+		for(int i =0; i<list.getLength();i++) {
+			attrs = list.item(i).getAttributes();
+			name=attrs.getNamedItem("name").getNodeValue();
+			numero=attrs.getNamedItem("numero").getNodeValue();
+			annuaire.put(name, numero);
+		}
+	}
+
+	@Override
+	public Numero get(String hotel) {
+		return new Numero(annuaire.get(hotel));
+	}
+
+	@Override
+	public Numero call(Object... params) throws IllegalArgumentException {
+	    return get(params[0].toString());
+	}
+
+}
--- src/hotel/LookForHotel.java
+++ src/hotel/LookForHotel.java
@@ -1,5 +1,14 @@
 package hotel;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import jus.aor.mobilagent.kernel.Agent;
+import jus.aor.mobilagent.kernel._Action;
+import jus.aor.mobilagent.kernel._Service;
+
 /**
  * J<i>ava</i> U<i>tilities</i> for S<i>tudents</i>
  */
@@ -8,9 +17,12 @@ package hotel;
  * Représente un client effectuant une requête lui permettant d'obtenir les numéros de téléphone des hôtels répondant à son critère de choix.
  * @author  Morat
  */
-public class LookForHotel{
+public class LookForHotel extends Agent {
+    private static final long serialVersionUID = -7856759896920597986L;
 	/** le critère de localisaton choisi */
 	private String localisation;
+	private List<Hotel> hotels;
+	private Map<Hotel,Numero> numeros;
 	// ...
 	/**
 	 * Définition de l'objet représentant l'interrogation.
@@ -19,15 +31,34 @@ public class LookForHotel{
 	 */
 	public LookForHotel(String... args){
 		localisation = args[0];
-	}
-	/**
-	 * réalise une intérrogation
-	 * @return la durée de l'interrogation
-	 * @throws RemoteException
-	 */
-	public long call() {
-		// ...
+		hotels = new LinkedList<>();
+		numeros = new HashMap<Hotel,Numero>();
 	}
 
-	// ...
+	protected _Action findHotel = new _Action(){
+
+	    private static final long serialVersionUID = -5462249915229967567L;
+
+	    @Override
+	    public void execute() {
+		List<Hotel> chaineHotels = (List<Hotel>) LookForHotel.this.getService("Hotels").call(new Object[]{localisation});
+		hotels.addAll(chaineHotels);
+	    }
+	    
+	};
+	
+	protected _Action findTelephone = new _Action(){
+
+	    private static final long serialVersionUID = -9211118182369995431L;
+
+	    @Override
+	    public void execute() {
+		_Service<Numero> annuaire = (_Service<Numero>) LookForHotel.this.getService("Telephones");
+		for(Hotel h : hotels){
+		    Numero n = annuaire.call(new Object[]{h.name});
+		    numeros.put(h, n);
+		}
+	    }
+	    
+	};
 }
