2ac6f7928afd1891331ffa712603abc1951af755
romanegarou <romanegallier@gmail.com>
2017-04-04 15:09:44 +0200

    modification agentserver

--- src/jus/aor/mobilagent/kernel/AgentServer.java
+++ src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -8,10 +8,14 @@ package jus.aor.mobilagent.kernel;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -32,26 +36,54 @@ public class AgentServer extends Thread {
     }
     
     
-    public _Agent getAgent(Socket s) throws IOException, ClassNotFoundException{
-        BAMAgentClassLoader classLoader = new BAMAgentClassLoader(this.getClass().getClassLoader());
-        InputStream is= s.getInputStream();
-        ObjectInputStream ois = new ObjectInputStream(is);
-        AgentInputStream ais= new AgentInputStream(is, classLoader);
+    public _Agent getAgent(Socket s){
+        try {
+            BAMAgentClassLoader classLoader = new BAMAgentClassLoader(this.getClass().getClassLoader());
+            InputStream is= s.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            AgentInputStream ais= new AgentInputStream(is, classLoader);
+
+            Jar jarjar = (Jar) ois.readObject();
+            classLoader.integrateCode(jarjar);
+
         
-        Jar jarjar = (Jar) ois.readObject();
-        classLoader.integrateCode(jarjar);
+            _Agent agent = (_Agent)ais.readObject();
+            ois.close();
+            ais.close();
+            is.close();
+            return agent;
+        } catch (IOException ex) {
+            Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        _Agent agent = (_Agent)ais.readObject();
         
-        ois.close();
-        ais.close();
-        is.close();
+        return null;
         
-        return _Agent;
     }   
     
     @Override
     public void run(){
+        boolean b= true;
+        ServerSocket socketServer;
+        try {
+            socketServer = new ServerSocket(port);
+            Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE, "Lancement de l'agent Server");
+            
+            while (b){
+                Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE,  "chargement d'un agent ");
+                Socket socketclient = socketServer.accept();
+                Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE, "reception de l'agent");
+                _Agent agent= this.getAgent(socketclient);
+                agent.reInit(this, name);
+                new Thread (agent).start();
+                socketclient.close();
+                }
+            socketServer.close();
+        } catch (IOException ex) {
+            Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
@@ -68,12 +100,16 @@ public class AgentServer extends Thread {
     }
     
     public _Service<?> getService(String s){
-        return null;
-        //TODO
+        return this.services.get(s);
     }
     
     public URI site(){
-        //TODO
+        
+        try {
+            return new URI("localhost:"+this.port);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AgentServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
 }
